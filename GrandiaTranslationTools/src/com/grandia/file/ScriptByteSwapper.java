47edaa2c5e158d7186c81b9be678c2943f241c7a
TrekkiesUnite118 <Legodude_118@yahoo.com>
2018-10-15 00:23:53 -0400

    Initial Commit.

---
+++ GrandiaTranslationTools/src/com/grandia/file/ScriptByteSwapper.java
@@ -0,0 +1,209 @@
+package com.grandia.file;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.grandia.file.utils.FileUtils;
+
+/**
+ * 
+ * ScriptByteSwapper
+ * 
+ *  This class will take the extracted Script files from the Playstation version
+ * of Grandia and swap their byte order from Little Endian to Big Endian. This is necessary for 
+ * the script to be usable in the Saturn version.
+ * 
+ * @author TrekkiesUnite118
+ *
+ */
+public class ScriptByteSwapper {
+
+	private static final Logger log = Logger.getLogger(ScriptByteSwapper.class.getName());
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	private List<File> fileList = new ArrayList<>();
+	private String inputFilePath;
+	private String outputFilePath;
+	private String fileExtension;
+	
+	public ScriptByteSwapper inputFilePath(String inputFilePath) {
+		this.inputFilePath = inputFilePath;
+		return this;
+	}
+	
+	public ScriptByteSwapper outputFilePath(String outputFilePath) {
+		this.outputFilePath = outputFilePath;
+		return this;
+	}
+	
+	public ScriptByteSwapper fileExtension(String fileExtension) {
+		this.fileExtension = fileExtension;
+		return this;
+	}
+	
+	public List<File> getFileList() {
+		return fileList;
+	}
+
+	public void setFileList(List<File> fileList) {
+		this.fileList = fileList;
+	}
+
+	public String getInputFilePath() {
+		return inputFilePath;
+	}
+
+	public void setInputFilePath(String inputFilePath) {
+		this.inputFilePath = inputFilePath;
+	}
+
+	public String getOutputFilePath() {
+		return outputFilePath;
+	}
+
+	public void setOutputFilePath(String outputFilePath) {
+		this.outputFilePath = outputFilePath;
+	}
+
+	public String getFileExtension() {
+		return fileExtension;
+	}
+
+	public void setFileExtension(String fileExtension) {
+		this.fileExtension = fileExtension;
+	}
+	
+	/**
+	 * Swaps the byte order of the files it reads in from the inputFilePath.
+	 * Currently only supports changing from Little Endian to Big Endian.
+	 * 
+	 */
+	public void swapBytes() {
+		if(inputFilePath == null) {
+			log.log(Level.WARNING, "Input File path is null, aborting parsing.");
+		} else {
+			populateFileArray();
+			for(File f : fileList) {
+				try {
+					byte[] fileBytes = Files.readAllBytes(f.toPath());
+					fileBytes = converting(fileBytes);
+					FileUtils.writeToFile(fileBytes, f.getName(), outputFilePath);
+				}catch (IOException e) {
+					log.log(Level.SEVERE, "Caught IOException attempting to read bytes.", e);
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Swaps the byte order for the text portions of the script back to their original order.
+	 * 
+	 * The swapBytes method swaps the entire file's order, however the text and control code
+	 * portions of the script were already in the proper order. So this method will switch
+	 * those portions back to their proper order.
+	 */
+	public void swapTextBytes() {
+		if(inputFilePath == null) {
+			log.log(Level.WARNING, "Input File path is null, aborting parsing.");
+		} else {
+			populateFileArray();
+			for(File f : fileList) {
+				try {
+					byte[] fileBytes = Files.readAllBytes(f.toPath());
+					ByteBuffer.wrap(fileBytes);
+					int length = fileBytes.length;
+
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					int i = 0;
+					while (i + 1 < length) {
+						byte[] sizeByte = new byte[2];
+						sizeByte[0] = fileBytes[i];
+						sizeByte[1] = fileBytes[i+1];
+						
+						out.write(sizeByte, 0, sizeByte.length);
+						
+						String hexValue = bytesToHex(sizeByte);
+						System.out.println("HexValue: " + hexValue);
+						if(hexValue.startsWith("9") || hexValue.startsWith("2")) {
+							System.out.println("HexValue Matched!");
+							int size = Integer.parseInt(hexValue.substring(1), 16);
+							System.out.println("Size is : " + size);
+							byte[] textPortion = new byte[size];
+							new String(textPortion);
+							if(i + 2 < length) {
+								copyBytes(fileBytes, textPortion, i+2, 0, size);
+								System.out.println("TextPortion: " + new String(textPortion));
+								textPortion = converting(textPortion);
+								System.out.println("TextPortion: " + new String(textPortion));
+								
+								out.write(textPortion, 0, size);
+							}
+							
+							size +=2;
+							i+=size;
+						}else {
+							i+=2;
+						}
+						
+						
+					}
+			
+					FileUtils.writeToFile(out.toByteArray(), f.getName(), outputFilePath);
+				}catch (IOException e) {
+					log.log(Level.SEVERE, "Caught IOException attempting to read bytes.", e);
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	private void copyBytes(byte[] from, byte[] to, int startFrom , int startTo, int size) {
+	
+		for(int i = startFrom; i <= startFrom + size ; i++) {
+			if(i < from.length && startTo < to.length) {
+				//System.out.println("Copying Bytes...");
+				to[startTo] = from[i];
+				startTo++;
+			}
+		}
+		
+	}
+
+	private static byte[] converting(byte[] value) {
+	    final int length = value.length;
+	    byte[] res = new byte[length];
+	    int i = 0;
+	    while(i < length) {
+	    	if(i+1 >= length) {
+	    		res[i] = value[i];
+	    	}else {
+		    	res[i] = value[i+1];
+		    	res[i+1] = value[i];
+	    	}
+	    	
+	        i += 2;
+	    }
+	    return res;
+	}
+	
+	private static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+	
+	private void populateFileArray() {
+		FileUtils.populateFileArray(fileList, inputFilePath, fileExtension);
+	}
+}
---
+++ GrandiaTranslationTools/src/com/grandia/file/structure/ScriptPointerTable.java
@@ -0,0 +1,142 @@
+package com.grandia.file.structure;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 
+ * ScriptPointerTable Class
+ * 
+ * This class represents the PointerTable used for the script portions of the MDT/MDP files.
+ * 
+ * @author TrekkiesUnite118
+ *
+ */
+public class ScriptPointerTable {
+	
+	private Map<Integer, PointerTableEntry> pointerTable = new HashMap<>();
+	private static final Logger log = Logger.getLogger(PointerTable.class.getName());
+	
+	public void addOrUpdatePointerTableEntry(int key, PointerTableEntry pte) {
+		pointerTable.put(key, pte);
+	}
+	
+	public PointerTableEntry getPointerTableEntry(Integer key) {
+		return pointerTable.get(key);
+	}
+	
+	public int getSize() {
+		return pointerTable.size();
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("POINTER TABLE: \n");
+		
+		for(Integer key : pointerTable.keySet()) {
+			sb.append("KEY: " + Integer.toHexString(key.intValue()) + "\n");
+			sb.append("TABLE ENTRY: " + pointerTable.get(key).toString());
+		}
+		
+		sb.append("SIZE: " + pointerTable.size() + " entires.");
+		
+		return sb.toString();
+	}
+	
+	/**
+	 * This Parses the pointer table into an easier to use object.
+	 * We convert the byte array into a ByteBuffer object, and then
+	 * use that to read off 4 bytes for the offset value.
+	 * Next we read the next 4 bytes for the size of the entry.
+	 * We then use those values plus the current value of the iterator
+	 * to create a PointerTableEntry Object to put in our PointerTable.
+	 * 
+	 * @param headerArray
+	 */
+	public void parsePointerTableFromByteArray(byte[] headerArray, boolean bigEndian) {
+		
+		log.log(Level.INFO, "Header size is : " + headerArray.length);
+		
+		//First we check that the header array is divisible by 4. If it's not, then we've made a mistake in getting it out of the file.
+		if(headerArray.length % 4 == 0) {
+			int i = 0;
+			int key = 0;
+			while(i < headerArray.length) {
+				
+				//Create a new pointer table entry
+				PointerTableEntry pte = new PointerTableEntry().tableOffset(i);
+				
+				//Read 2 bytes out of the Header Array into a temporary buffer.
+				ByteBuffer bb = ByteBuffer.wrap(headerArray, i, 2);
+				
+				/*
+				 * If we're not dealing with the Saturn (Big Endian), then we're dealing with the PS1.
+				 * So it needs to  be Little Endian.
+				 */
+				if(!bigEndian) {
+					bb.order(ByteOrder.LITTLE_ENDIAN);
+				}
+				
+				//Set the id in the pointer table entry to the bytes we just read off.
+				pte.id(Short.toUnsignedInt(bb.getShort()));
+				
+				//Move ahead 2 bytes and read.
+				i += 2;
+				bb = ByteBuffer.wrap(headerArray, i, 2);
+				
+				/*
+				 * Set the endianess.
+				 */
+				if(!bigEndian) {
+					bb.order(ByteOrder.LITTLE_ENDIAN);
+				}
+				
+				//Set the offset to the bytes we just read.
+				pte.offset(Short.toUnsignedInt(bb.getShort()));
+				
+				//Move ahead 2 bytes.
+				i += 2;
+				
+				//Add the new PointerTableEntry to the PointerTable.
+				this.addOrUpdatePointerTableEntry(key, pte);
+				key++;
+			}
+			calculateEntrySize();
+		} else {
+			log.log(Level.SEVERE, "Header is not divisible by 4! We've made a mistake in parsing it.");
+		}
+	}
+	
+	public void calculateEntrySize() {
+		for(int i = 0; i < pointerTable.size(); i++) {
+			
+			if(i + 1 < pointerTable.size()) {
+				PointerTableEntry pte = pointerTable.get(i);
+				PointerTableEntry nextPte = pointerTable.get(i+1);
+				
+				if(!Integer.toHexString(nextPte.getOffset()).equals("ffff")) {
+					int size = nextPte.getOffset() - pte.getOffset();
+					pte.setSize(size);
+					pointerTable.put(i,  pte);
+				}
+			}
+		}
+	}
+	
+	public void recacluateOffsets() {
+		int offset = 0;
+		for(int i = 0; i < pointerTable.size(); i++) {
+			PointerTableEntry pte = pointerTable.get(i);
+			if(!Integer.toHexString(pte.getId()).equals("ffff")) {
+				pte.setOffset(offset);
+				pointerTable.put(i, pte);
+				offset += pte.getSize();
+			}
+		}
+	}
+}
